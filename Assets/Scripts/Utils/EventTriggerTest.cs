090c56d35a11717dc9757be7eae95db59767f041
Sergej Frank <git@sergej-frank.de>
2019-10-04 22:29:06 +0200

    listen to any char

--- Assets/Scripts/Utils/EventTriggerTest.cs
+++ Assets/Scripts/Utils/EventTriggerTest.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EventTriggerTest : MonoBehaviour
 {
+
+    private static readonly KeyCode[] allowedKeys = {
+        KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E
+    };
     // Start is called before the first frame update
-    void Update()
-    {
-        if (Input.GetKeyDown("q"))
-        {
-            Debug.Log("trigger q");
-            EventManager.TriggerEvent("typo",'q');
+    void Update() {
+        if (Input.anyKey && Input.inputString.Length > 0){
+            Debug.Log("inputstring: " + Input.inputString);
+            char key = Input.inputString.ToLower()[0];
+            if (Char.IsLetter(key)){
+                Debug.Log("triggerKey " + Input.inputString);
+                EventManager.TriggerEvent("typo", key);
+            }
+
         }
 
     }
