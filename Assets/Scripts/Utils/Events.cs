d46caab28213aa2560b24cb11be337319c2d059f
Jim Duden <jim.duden@otto.de>
2019-10-04 22:43:11 +0200

    add keycode enum

---
+++ Assets/Scripts/Utils/Events.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Events {
+    public static String KEY_DOWN="keyDown",
+    public static String TYPO="typo",
+    public static String GAME_OVER="gameOver"
+};
