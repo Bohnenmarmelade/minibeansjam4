8dd36589f251dad356fc9fdd663308c35ae7ca88
Roman Lamsal <roman.lamsal@otto.de>
2019-10-04 13:12:21 +0200

    .gitignore for Rider; Tests- and Scripts-directory with assemblies

---
+++ Assets/Tests/FooTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class FooTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void FooTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Assert.AreEqual("foo", "foo");
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator FooTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
