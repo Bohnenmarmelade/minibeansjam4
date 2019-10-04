e0e3919740061a2b7aef697dd621d9ecec9f9045
Jim Duden <jim.duden@otto.de>
2019-10-04 22:49:22 +0200

    delete translator and fix event enum

--- Assets/Scripts/Utils/Events.cs
+++ Assets/Scripts/Utils/Events.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class Events {
-    public static String KEY_DOWN="keyDown",
-    public static String TYPO="typo",
-    public static String GAME_OVER="gameOver"
+    public static string KEY_DOWN="keyDown";
+    public static string TYPO="typo";
+    public static string GAME_OVER="gameOver";
 };
