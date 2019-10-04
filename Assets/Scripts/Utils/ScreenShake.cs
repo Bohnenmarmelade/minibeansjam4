b70b9f5834aff8f4ed4c69d8bfe02d03c5bd3920
bttrs <luca@luca.wtf>
2019-10-04 23:27:58 +0200

    add itween plugin for camera movement && fit canvas to camera size

---
+++ Assets/Scripts/Utils/ScreenShake.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShake : MonoBehaviour
+{
+    public int shakeMultiplier = 2;
+    public int shakeLength = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //iTween.ShakePosition(gameObject, new Vector3(.1f * shakeMultiplier, .1f * shakeMultiplier, .1f * shakeMultiplier), 1 * shakeLength);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
