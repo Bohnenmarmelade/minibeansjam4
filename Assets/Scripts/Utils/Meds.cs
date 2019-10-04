19536d15a62c3727398b277fd271929c9763bcaf
Sergej Frank <git@sergej-frank.de>
2019-10-04 23:53:18 +0200

    oopsi

--- Assets/Scripts/Utils/Meds.cs
+++ Assets/Scripts/Utils/Meds.cs
@@ -31,14 +31,14 @@ public static class Meds
     };
 
 
-    public static string generateMed(Difficulty difficulty){
+    public static string getMed(string difficulty){
         int randSuffix = Random.Range(0, Meds.SUFFIX.Length);
         string suffix = Meds.SUFFIX[randSuffix];
 
         string prefix = "";
         if(difficulty.Equals(Difficulty.EASY)){
             prefix = Meds.EASY[Random.Range(0, Meds.EASY.Length)];
-        } else if(){
+        } else if(difficulty.Equals(Difficulty.MEDIUM)){
             prefix = Meds.MEDIUM[Random.Range(0, Meds.MEDIUM.Length)];
         } else {
             prefix = Meds.HARD[Random.Range(0, Meds.HARD.Length)];
