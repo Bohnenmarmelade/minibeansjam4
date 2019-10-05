11847eea2c73f08bfcc86ee4bf8d4956661bed92
Sergej Frank <git@sergej-frank.de>
2019-10-05 14:06:29 +0200

    added shake on typing error

--- Assets/Scripts/Utils/Events.cs
+++ Assets/Scripts/Utils/Events.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Events
-{
-    public const string KEY_DOWN = "keyDown";
-    public const string TYPO = "typo";
-    public const string GAME_OVER = "gameOver";
-    public const string BOTTLE_SUCCES = "bottleSuccess";
+public static class Events {
+    public const string KEY_DOWN="keyDown";
+    public const string TYPO="typo";
+    public const string GAME_OVER="gameOver";
+    public const string BOTTLE_SUCCES="bottleSuccess";
+    public const string SHAKE = "shake";
     public const string INCREASE_DIFFICULTY = "increaseDifficulty";
 };
