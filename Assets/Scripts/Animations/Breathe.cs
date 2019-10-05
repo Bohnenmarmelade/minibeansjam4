06257199de68e72a8d5ade3c7acef604e3b45e3e
Jim Duden <jim.duden@otto.de>
2019-10-05 22:44:36 +0200

    implement breathing movement

---
+++ Assets/Scripts/Animations/Breathe.cs
@@ -0,0 +1,19 @@
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    public class Breathe : MonoBehaviour
+    {
+        Vector3 startPos;
+        public const float amplitude = 0.5f;
+        public const float period = 2f;
+        protected void Start()
+        {
+            startPos = transform.position;
+        }
+        protected void Update()
+        {
+            float theta = Time.time / period;
+            float distance = amplitude * Mathf.Sin(theta);
+            transform.position = startPos + Vector3.up * distance;
+        }
+    }
