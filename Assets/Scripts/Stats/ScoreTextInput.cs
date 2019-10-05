265b8ec515bd70aec953f2b2a4f664a536b582a8
Jim Duden <jim.duden@otto.de>
2019-10-05 17:06:09 +0200

    get global score

---
+++ Assets/Scripts/Stats/ScoreTextInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utils;
+public class ScoreTextInput : MonoBehaviour
+{
+    public Text scoreText;
+
+    private void OnEnable() {
+        SetTextFieldsContent();
+    }
+    private void SetTextFieldsContent()
+    {   
+        //float test = GameObject.FindGameObjectsWithTag("_app")[0].transform.position[0];
+        GameStatsController test = GameObject.FindGameObjectsWithTag("_app")[0].GetComponent(typeof(GameStatsController)) as GameStatsController;
+        scoreText.text = "<b>" + "Your Score is: " + test.score + "</b>";
+    }
+}
