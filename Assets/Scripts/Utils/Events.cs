6d3e1b232424372c7040ab81f89a642cc8deef88
Jim Duden <jim.duden@otto.de>
2019-10-05 21:20:47 +0200

    add highscore

--- Assets/Scripts/Utils/Events.cs
+++ Assets/Scripts/Utils/Events.cs
@@ -10,4 +10,5 @@ public static class Events {
     public const string SHAKE = "shake";
     public const string INCREASE_DIFFICULTY = "increaseDifficulty";
     public const string START_GAME = "startGame";
+    public const string SHOW_HIGHSCORE = "showHighscore";
 };
