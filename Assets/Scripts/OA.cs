1aabfd84cef2493c9fe3120de4bbefbc3d20f373
bttrs <luca@luca.wtf>
2019-10-06 18:17:13 +0200

    add oa scene

---
+++ Assets/Scripts/OA.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OA : MonoBehaviour
+{
+    public int wait = 5;
+    private bool sceneChanged = false;
+    private AudioSource oaSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oaSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!oaSource.isPlaying && !sceneChanged)
+        {
+            StartCoroutine("startOA");
+        }
+    }
+
+    IEnumerator startOA()
+    {
+        sceneChanged = true;
+        yield return new WaitForSeconds(wait);
+        Debug.Log("change");
+        SceneManager.LoadScene("_preload");
+    }
+}
